0a961e9eb0c7a0cbdf376a088fcff6addba9e6b8
dheer152004 <dheerajkumar152004@outlook.com>
2026-01-20 11:39:28 +0530

    We will make ZenTask To ZenTask AI

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -67,7 +67,7 @@ export const Sidebar: React.FC<SidebarProps> = ({
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
           </svg>
         </div>
-        <h1 className="text-xl font-bold tracking-tight text-slate-800 dark:text-slate-100">ZenTask <span className="text-indigo-600">AI</span></h1>
+        <h1 className="text-xl font-bold tracking-tight text-slate-800 dark:text-slate-100">ZenTask <span className="text-indigo-600"></span></h1>
       </div>
 
       <nav className="w-full space-y-1 mb-8 flex-shrink-0">
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import { exp } from 'firebase/firestore/pipelines';
 
 //for local pc
 export default defineConfig(({ mode }) => {
